915e7b388879ec5ad9b704d4f1b5386d86a7fa75
AJ Caldwell <tomatrow@gmail.com>
2016-08-07 16:55:33 -0700

    Planned test interface.

--- test.hpp
+++ test.hpp
@@ -1,6 +1,18 @@
 #ifndef TEST_HPP
 #define TEST_HPP
 
+#include <string>
+
+using namespace std;
+
 int main(int argc, char const *argv[]);
+
+/**
+* For both checkpoint1.txt and checkpoint2.txt
+*   Calls compress
+*   calls uncompress
+*   Diff's between original.
+*/
+void checkPointTest(string location);
 
 #endif // TEST_HPP
