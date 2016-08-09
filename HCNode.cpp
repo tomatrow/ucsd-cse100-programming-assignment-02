220b23877eb6038088da6e4f8cae082e37a1933e
AJ Caldwell <tomatrow@gmail.com>
2016-08-09 15:37:49 -0700

    Adhereing to the hints.

--- HCNode.cpp
+++ HCNode.cpp
@@ -7,7 +7,7 @@
 bool HCNode::operator<(const HCNode& other)
 {
 	if (this->count != other.count) {
-		return this->count < other.count;
+		return this->count > other.count;
 	}
 
 	return this->symbol < other.symbol;
