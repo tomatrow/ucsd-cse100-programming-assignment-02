27b3cd73fbc69bc4341ffc7088598c6ff5215266
AJ Caldwell <tomatrow@gmail.com>
2016-08-09 11:27:38 -0700

    Implemented node ordering. Not if scope is proper.

--- HCNode.cpp
+++ HCNode.cpp
@@ -1,3 +1,23 @@
 #ifndef HCNODE_CPP
 #define HCNODE_CPP
+
+#include <cassert>
+#include "HCNode.hpp"
+
+bool HCNode::operator<(const HCNode& other)
+{
+	if (this->count != other.count) {
+		return this->count < other.count;
+	}
+
+	return this->symbol < other.symbol;
+}
+
+bool comp(HCNode* one, HCNode* other)
+{
+	assert(one != nullptr && other != nullptr);
+	return *one < *other;
+}
+
+
 #endif // HCNODE_CPP
