6f35a34ee1877c3c5f104e2531c693328e5c10e3
AJ Caldwell <tomatrow@gmail.com>
2016-08-11 19:58:20 -0700

    Implemented compression/decompression

--- compress.cpp
+++ compress.cpp
@@ -1,7 +1,27 @@
 #ifndef COMPRESS_CPP
 #define COMPRESS_CPP
 
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <vector>
+#include "HCTree.hpp"
+#include "FileUtility.hpp"
+
+using namespace std;
+
 int main(int argc, char* argv[]) {
+	// cout << "Hello World!!! (compress)" << endl;
+
+	assert(argc == 3); // path original destination
+
+	FileUtility utility(argv[1], argv[2]);
+	vector<int> freq(256);
+	HCTree::frequencies(utility.inFileStream(), freq);
+	HCTree tree;
+	tree.build(freq);
+	tree.encode(utility.inFileStream(), utility.outFileStream());
+
 	return 0;
 }
 
--- uncompress.cpp
+++ uncompress.cpp
@@ -1,7 +1,24 @@
 #ifndef UNCOMPRESS_CPP
 #define UNCOMPRESS_CPP
 
+#include <iostream>
+#include <cassert>
+#include <fstream>
+#include <vector>
+#include "HCTree.hpp"
+#include "FileUtility.hpp"
+
 int main(int argc, char* argv[]) {
+	// cout << "Hello World!!! (uncompress)" << endl;
+	assert(argc == 3); // path original destination
+
+	FileUtility utility(argv[1], argv[2]);
+	vector<int> freq(256);
+	HCTree::decodeHeader(utility.inFileStream(), freq);
+	HCTree tree;
+	tree.build(freq);
+	tree.decode(utility.inFileStream(), utility.outFileStream());
+
 	return 0;
 }
 
